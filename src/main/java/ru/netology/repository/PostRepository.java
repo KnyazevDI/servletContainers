00cf300dc2230edc7f56ddbd9be57b3a470cb615
Дмитрий Князев <knyazevdi87@yandex.ru>
2023-06-08 22:55:52 +0300

    initial commit

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,42 @@
+package ru.netology.repository;
+
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+// Stub
+public class PostRepository {
+    private final AtomicLong idPost = new AtomicLong();
+    private final Map<Long, Post> postMap = new ConcurrentHashMap<>();
+
+    public List<Post> all() {
+        return new ArrayList<>(postMap.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(postMap.get(id));
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            var id = idPost.incrementAndGet();
+            post.setId(id);
+            postMap.put(post.getId(), post);
+        }
+        if (postMap.containsKey(post.getId())) {
+            postMap.put(post.getId(), post);
+        } else {
+            throw new NotFoundException();
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        if (postMap.containsKey(id)) {
+            postMap.remove(id);
+        } else throw new NotFoundException();
+    }
+}
---
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,59 @@
+package ru.netology.servlet;
+
+import ru.netology.controller.PostController;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+    private PostController controller;
+    private static final String GET = "GET";
+    private static final String POST = "POST";
+    private static final String DELETE = "DELETE";
+    private static final String POSTS_PATH = "/api/posts";
+    private static final String POST_PATH = "/api/posts/\\d+";
+
+    @Override
+    public void init() {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        controller = new PostController(service);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+
+            if (method.equals(GET) && path.equals(POSTS_PATH)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals(GET) && path.matches(POST_PATH)) {
+                controller.getById(getId(path), resp);
+                return;
+            }
+            if (method.equals(POST) && path.equals(POSTS_PATH)) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals(DELETE) && path.matches(POST_PATH)) {
+                controller.removeById(getId(path), resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private static long getId(String path) {
+        return Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+    }
+}
+
